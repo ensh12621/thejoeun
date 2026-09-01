3901791f1ac2f3057a3e61abaae23abbc04772e2
TJ-BU-708-023 <TJ-BU-708-023@DESKTOP-U3RN6JI>
2026-09-01 18:04:04 +0900

    커밋2

--- javaProj1/src/day2/_19_중첩반복문.java
+++ javaProj1/src/day2/_19_중첩반복문.java
@@ -4,10 +4,11 @@ public class _19_중첩반복문 {
 
 	public static void main(String[] args) {
 		for(int i=1; i<=10; i++) {
+			System.out.println("============== " + i + " ================");
 			for(int j=1; j<=10; j++) {
 				System.out.print(j + " ");
 			}
-			System.out.println();
+			System.out.println("\n");
 		}
 	}
 }
---
+++ javaProj1/src/day2/_21_중첩반복연습문제.java
@@ -0,0 +1,26 @@
+package day2;
+
+import java.util.Scanner;
+
+public class _21_중첩반복연습문제 {
+
+	public static void main(String[] args) {
+		
+		for(int i=9; i>=2; i--) {
+			for(int j=9; j>=1; j--) {
+				System.out.println(i + " x " + j + " = " + (i *j));
+			}
+			System.out.println();
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("별 갯수: ");
+		int starNum = scanner.nextInt();
+		for(int i=0; i<starNum; i++) {
+			for(int j=i; j>=0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
